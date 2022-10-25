318c67dd22098608585c20bdf27ab8ff52ac748e
ANALISIS DE SISTEMAS <ANALISIS DE SISTEMAS@DESKTOP-I3DMA97>
2022-10-24 21:27:38 -0500

    Agregar archivos de proyecto.

---
+++ hola/Login.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace hola
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIniciar_Clicked(object sender, EventArgs e)
+        {
+            string usuario = "vladi";
+            string contrasena = "1234";
+
+            string tUsuario = txtUsuario.Text;
+            string tContrasena = txtContrasena.Text;
+
+            if (usuario == tUsuario & contrasena == tContrasena)
+            {
+                DisplayAlert("Alerta", "USUARIO CORRECTO", "Cerrar");
+            }
+            else
+            {
+                DisplayAlert("Alerta", "USUARIO NO EXISTE", "Cerrar");
+            }
+
+      
+        }
+    }
+}
